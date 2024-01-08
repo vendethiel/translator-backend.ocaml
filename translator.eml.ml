666e849a61c4a1a14b3e355c6d47dbaea83fb2cf
vendethiel <vendethiel@hotmail.fr>
2024-01-08 22:52:43 +0100

    find project by id

--- translator.eml.ml
+++ translator.eml.ml
@@ -14,17 +14,31 @@ type project_api = {
   name : string;
 } [@@deriving yojson]
 
+
+let to_project_object (id, name) =
+  { id; name }
+
 let list_projects =
   let query =
     let open Caqti_request.Infix in
     T.(unit ->* tup2 int string)
     "SELECT rowid, name FROM project" in
-  let to_project_object (id, name) =
-    { id; name } in
   fun (module Db : DB) ->
     let%lwt projects_or_error = Db.collect_list query () in
     let%lwt projects = Caqti_lwt.or_fail projects_or_error in
     Lwt.return @@ List.map to_project_object projects
+
+let find_project =
+  let query =
+    let open Caqti_request.Infix in
+    T.(int ->! tup2 int string)
+    "SELECT rowid, name FROM project WHERE rowid = $1" in
+  fun id (module Db : DB) ->
+    let%lwt project_or_error = Db.find query id in
+    let%lwt project = Caqti_lwt.or_fail project_or_error in
+    Lwt.return @@ to_project_object project
+
+
 let cors_middleware handler req =
   let handlers =
     [ "Allow", "OPTIONS, GET, HEAD, POST"
@@ -39,6 +53,7 @@ let cors_middleware handler req =
   |> List.iter (fun (key, value) -> Dream.add_header res key value);
   Lwt.return res
 
+
 let () =
   Dream.run
   @@ Dream.logger
@@ -51,6 +66,19 @@ let () =
       |> yojson_of_list yojson_of_project_object
       |> Yojson.Safe.to_string
       |> Dream.json);
+
+    Dream.get "/projects/:id" (fun request ->
+      let id_param = Dream.param request "id" in
+      match int_of_string_opt id_param with
+      | None ->
+          Dream.empty `Bad_Request
+      | Some id ->
+          let%lwt project = Dream.sql request (find_project id) in
+          project
+          |> yojson_of_project_object
+          |> Yojson.Safe.to_string
+          |> Dream.json);
+
     Dream.post "/projects" (fun request ->
       let%lwt body = Dream.body request in
       let project_object =
